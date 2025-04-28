44e72b3189d545accac62dc522f814798903a4ed
Gjuro Kladarić <gjuro@outlook.com>
2025-04-28 21:51:55 +0200

    Old code removed. Code behind file created for BoardGame.

---
+++ BlazingTicTacToe/Pages/BoardGame.razor.cs
@@ -0,0 +1,14 @@
+namespace BlazingTicTacToe.Pages
+{
+    public partial class BoardGame
+    {
+        int rowClicked = 0;
+        int columnClicked = 0;
+
+        private void SquareCliked(int row, int col)
+        {
+            rowClicked = row;
+            columnClicked = col;
+        }
+    }
+}
